f192ab27bfe14d45f6d0322a025477b02ac19aeb
Jose <prueba@prueba.com>
2013-03-14 18:44:00 +0100

    Version2 OK

---
+++ cities.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="es" xml:lang="es">
+ <head>
+   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+     
+   <title>MySQL and PHP</title>
+   
+</head>
+<body>
+<h1>Cities of the world</h1>
+<?php
+	require("connection_cities.php");
+	while ($row = $query_results->fetch_assoc()){
+		$dir = "city.php?id=".$row['ID'];
+		echo $row["ID"]," - ","<a href=".$dir.">",$row['Name'],"</a><br/>";
+	}
+	
+	$query_results->close();
+	
+	$linkID1->close();
+?>
+
+</body>
+</html>
